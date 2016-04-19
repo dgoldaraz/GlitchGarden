8d1dcf76383282a35b9fe42e1e93103895219c20
dgoldaraz <dgoldaraz@gmail.com>
2016-04-19 23:20:21 +0100

    Sound Manager and Aspect Ratio
    Add a new Musin Manager
    Add Sounds/images

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	//Array storing the different music for each level
+	public AudioClip[] levelMusicArray;
+
+	private AudioSource m_audioSource;
+	//-----------------------------------------------------------------------------------------------------
+	void Awake () 
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+	//-----------------------------------------------------------------------------------------------------
+	void Start () 
+	{
+		m_audioSource = GetComponent<AudioSource>();
+		if(!m_audioSource)
+		{
+			Debug.LogWarning("Warning, no audio source attached tpo music manager");
+		}
+	}
+	//-----------------------------------------------------------------------------------------------------
+	// Call it every time a level it's loaded
+	void OnLevelWasLoaded (int level) 
+	{
+			//Update music and level
+			AudioClip newMusic = levelMusicArray[level];
+			if(newMusic)
+			{
+				m_audioSource.Stop();
+				m_audioSource.clip = newMusic;
+				m_audioSource.Play();
+			}
+	}
+}
